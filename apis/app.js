7e25f37cf155d1b06923c8bfdc9131e624cf0635
alif338 <alifm26072000@gmail.com>
2021-10-01 06:16:28 +0700

    initial commit

---
+++ apis/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import User from './src/routes/user'
+import './src/config' // Menginisiasi MongoDB di file config.js
+require('dotenv').config({path: `./${process.env.NODE_ENV}.env`})
+const app = express()
+
+console.log(process.env.PORT)
+console.log(`./${process.env.NODE_ENV}.env`)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use('/users', User)
+app.listen(process.env.PORT, () => {
+    console.log(`App listening at http://127.0.0.1:${process.env.PORT}`)
+});
---
+++ apis/src/services/user/get.js
@@ -0,0 +1,7 @@
+import {User} from "../../models/user"
+
+export const all = async () => User.find({})
+
+export const byId = async (id) => User.findById(id)
+
+
